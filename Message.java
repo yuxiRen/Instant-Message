0baf85214e0c60af4d66948570f996fa8dbcd6b6
yuxiRen <rita.ren.yuxi@gmail.com>
2021-12-13 20:04:55 -0800

    initialize User and Message

---
+++ Message.java
@@ -0,0 +1,43 @@
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String sender;
+    private String receiver;
+    private String content;
+    private String sendTime;
+    private String type;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(String sendTime) {
+        this.sendTime = sendTime;
+    }
+
+}
